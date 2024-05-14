f2f0f9cdf761efe5112e777d16894c9eb631d514
AranganathanPrakash <114086134+AranganathanPrakash@users.noreply.github.com>
2024-05-14 13:38:27 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,15 @@ docker-compose up -d
 docker login -u ar8888  -p dckr_pat_NqD6cUw-P2zyV9GQ715-Y8hLK_I
 
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
+       sh'chmod +x build.sh'
+       sh'./build.sh'
 
         docker tag test ar8888/dev
         docker push vennilavan/dev
 
 if [[ $GIT_BRANCH == "origin/master" ]]; then
+        sh'chmod +x build.sh'
+        sh'./build.sh'
 
         docker tag test ar8888/prod
         docker push vennilavan/prod
